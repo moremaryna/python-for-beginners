511f6eb8212c7ed9b0a4d1a668f1d594ff7d57dc
moremaryna <mary.seleznova@gmail.com>
2024-01-20 13:06:44 +0000

    final version of Rock-Paper-Scissors-Beau-Carnes

--- main.py
+++ main.py
@@ -3,14 +3,32 @@ import random
 def get_choices():
   player_choice = input("Enter a choice - rock, paper, scissors: ")
   options = ["rock", "paper", "scissors"]
-
   computer_choice = random.choice(options)
   choices = {"player": player_choice, "computer": computer_choice}
   return choices
 
-def get_dinner():
+def check_win(player, computer):
+  print (f"You chose {player}, computer chose {computer}")
+  if player == computer:
+    return "It's a tie!"
+  elif player == "rock":
+    if computer == "scissors":
+      return "Rock smashes scissors! You win!"
+    else:
+      return "Paper covers rock. You lose!"
+  elif player == "paper":
+    if computer == "rock":
+      return "Rock smashes scissors! You win!"
+    else:
+      return "Schisers broken on rock. You lose!"
+  elif player == "schissors":
+    if computer == "rock":
+      return "Rock smashes scissors! You win!"
+  else:
+    return "Scissers broken on rock. You lose!"
 
-  food  = ["pizza", "carrots", "eggs"]
-  dinner = random.choice(food)
+choices = get_choices()
 
-  return get_dinner
+result = check_win(choices["player"], choices["computer"])
+
+print(result)
