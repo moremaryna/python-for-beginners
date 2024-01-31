b46e11a5828555a31f61eeff65ae180247863766
moremaryna <mary.seleznova@gmail.com>
2024-01-31 22:13:30 +0000

    New excersise from the book

---
+++ hello.py
@@ -0,0 +1,25 @@
+# Based on Python: All-in-one for dummies. 7 Books in one! John C. Shovic, Alan Simpson
+
+
+# This is a Python comment in my first Python app.
+
+
+"""This is a multiline comment in Python.
+This type of comment is sometimes called a docstring.
+A docstring  starts with three double-quotation marks, and also ends with three double quotation marks. """
+
+#This variable contanis an integer
+
+quantity = 10
+
+#This variable contains a float
+
+unit_price = 1.99
+
+#This variable contans the result of multiplying quantity times unit price
+
+extended_price = quantity * unit_price
+
+#Show the results
+print(extended_price)
+
