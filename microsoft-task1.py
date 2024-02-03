6f3e1866c78693daf07db8e7e4943b57d2de66b3
moremaryna <mary.seleznova@gmail.com>
2024-02-03 08:34:52 +0000

    A few exercises from Microsoft Learning Course

---
+++ microsoft-task1.py
@@ -0,0 +1,4 @@
+parsecs_input = input("Enter the number of Parsecs: ")
+parsecs = int(parsecs_input)
+lightyears = parsecs * 3.26
+print(str(parsecs_input) + " parsecs is " + str(lightyears) + " lightyears")
