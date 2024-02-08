6a247a5e68bf8e15edf34c5438da5289c1bc7c68
moremaryna <mary.seleznova@gmail.com>
2024-02-08 23:53:16 +0000

    new microsoft learning tasks

---
+++ microsoft-task8-store-numbers.py
@@ -0,0 +1,17 @@
+gravity_on_earth = 1.0
+gravity_on_the_moon = 0166
+
+gravity_on_planets = [0.378, 0.907, 2.36, .916, 0.889, 1.2]
+
+gravity_on_planets = [0.378, 0.907, 1, 0.377, 2.36, 0.916, 0.889, 1.12]
+bus_weight = 124054 # in Newtons, on Earth
+
+print("On Earth, a double-decker bus weighs", bus_weight, "N")
+print("On Mercury, a double-decker bus weighs", bus_weight * gravity_on_planets[0], "N")
+
+gravity_on_planets = [0.378, 0.907, 1, 0.377, 2.36, 0.916, 0.889, 1.12]
+bus_weight = 12650 # in Newtons, on Earth
+
+print("On Earth, a double-decker bus weighs", bus_weight, "N")
+print("The lightest a bus would be in the solar system is", bus_weight * min(gravity_on_planets), "N")
+print("The heaviest a bus would be in the solar system is", bus_weight * max(gravity_on_planets), "N")
---
+++ microsoft-task9-join-lists.py
@@ -0,0 +1,22 @@
+amalthea_group = ["Metis", "Adrastea", "Amalthea", "Thebe"]
+galilean_moons = ["Io", "Europa", "Ganymede", "Callisto"]
+
+regular_satellite_moons = amalthea_group + galilean_moons
+print("The regular satellite moons of Jupiter are", regular_satellite_moons)
+
+amalthea_group = ["Metis", "Adrastea", "Amalthea", "Thebe"]
+galilean_moons = ["Io", "Europa", "Ganymede", "Callisto"]
+
+regular_satellite_moons = amalthea_group + galilean_moons
+regular_satellite_moons.sort()
+print("The regular satellite moons of Jupiter are", regular_satellite_moons)
+
+amalthea_group = ["Metis", "Adrastea", "Amalthea", "Thebe"]
+galilean_moons = ["Io", "Europa", "Ganymede", "Callisto"]
+
+regular_satellite_moons = amalthea_group + galilean_moons
+regular_satellite_moons.sort(reverse=True)
+print("The regular satellite moons of Jupiter are", regular_satellite_moons)
+
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+user_planet = input("Write a name of planet (capital letter to start):")
