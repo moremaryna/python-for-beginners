3a8090f9dd21fc2c61802ee82457d1540f6c38b7
moremaryna <mary.seleznova@gmail.com>
2024-02-09 22:17:47 +0000

    new microsoft learning tasks

--- microsoft-task9-join-lists.py
+++ microsoft-task9-join-lists.py
@@ -18,5 +18,15 @@ regular_satellite_moons = amalthea_group + galilean_moons
 regular_satellite_moons.sort(reverse=True)
 print("The regular satellite moons of Jupiter are", regular_satellite_moons)
 
+# TASK
+
 planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
 user_planet = input("Write a name of planet (capital letter to start):")
+planet_index = planets.index(user_planet)
+
+print("here are the planets closer than " + user_planet)
+print(planets[0:planet_index])
+
+# Enter code below
+print("here are the planets further than" + user_planet)
+print(planets[planet_index+1])
