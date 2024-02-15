99991443199f09af56ed945299e541f187c92cda
moremaryna <mary.seleznova@gmail.com>
2024-02-15 23:51:54 +0000

    updates

---
+++ microsoft-task11-example.py
@@ -0,0 +1,9 @@
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+planets_before_earth = planets[0:2]
+print(planets_before_earth)
+
+planets_after_earth = planets[3:8]
+print(planets_after_earth)
+
+planets_after_earth = planets[3:]
+print(planets_after_earth)
