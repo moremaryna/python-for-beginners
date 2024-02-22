2247155c1c0ce7f9a1e7266a4ed51ddf84c870ce
moremaryna <mary.seleznova@gmail.com>
2024-02-22 23:19:17 +0000

    Create submission1.py

---
+++ submission1.py
@@ -0,0 +1,4 @@
+name = str(input("Enter your name: ")) # This is name of user
+email = str(input("Enter your email: ")) # This is email of user
+
+print("Hi " + name + "! " + "We will be contacting you shortly at "+ email) # Print the message "Hi $name! We will be contacting you shortly at $email"
