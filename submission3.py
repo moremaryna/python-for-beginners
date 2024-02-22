980c3db1b5709e70787554e61bbdf9ef770f98ce
moremaryna <mary.seleznova@gmail.com>
2024-02-22 23:34:42 +0000

    Create submission3.py

---
+++ submission3.py
@@ -0,0 +1,11 @@
+ladies = ["Alfa", "Bravo", "Charlie", "Delta", "Eco"]
+print(ladies) # Print list of the ladies
+
+ladies.insert(0, "Jenny")
+print(ladies) # Print Jenny in front of other lady
+
+ladies.remove(ladies[2])
+print(ladies) # Remove lady no.3
+
+ladies.append("Alice")
+print(ladies) # Add Alice to the queue
