b41b0e7643c29b8174239020e011f708620a7747
moremaryna <mary.seleznova@gmail.com>
2024-02-28 23:48:55 +0000

    update

--- microsoft-task12-example.py
+++ microsoft-task12-example.py
@@ -1,9 +1,1 @@
-planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
-planets_before_earth = planets[0:2]
-print(planets_before_earth)
-
-planets_after_earth = planets[3:8]
-print(planets_after_earth)
-
-planets_after_earth = planets[3:]
-print(planets_after_earth)
+#task for 12 submission
